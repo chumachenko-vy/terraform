e44de4c8e62b6f76567ca7fd400741697b5d3a2b
Volodymyr Chumachenko <volodymyr.chumachenko@aspira.global>
2024-09-15 16:12:23 +0300

    fixes in bash script

---
+++ lesson-4/webserver.tf
@@ -0,0 +1,47 @@
+resource "aws_instance" "webserver" {
+  ami           = "ami-0c6da69dd16f45f72" # amazon linux
+  instance_type = "t3.micro"
+  tags = {
+    Name = "Apache-dynamic"
+  }
+  vpc_security_group_ids = [aws_security_group.webserver.id]
+  user_data = templatefile("user_data.sh.tpl", {
+    f_name = "Vald",
+    l_name = "Ch",
+    names  = ["Vasya", "petya", "Donald"]
+  })
+}
+
+resource "aws_security_group" "webserver" {
+  name        = "webserver security group"
+  description = "security group for web server"
+
+  ingress {
+    from_port   = "80"
+    to_port     = "80"
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = "443"
+    to_port     = "443"
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = "22"
+    to_port     = "22"
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = "0"
+    to_port     = "0"
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+
+  }
+
+}
