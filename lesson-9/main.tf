001ce6ca2538cef2c70ac60eebcee1eadf4354bd
Volodymyr Chumachenko <volodymyr.chumachenko@aspira.global>
2024-09-18 20:54:16 +0300

    add two subnets

--- lesson-9/main.tf
+++ lesson-9/main.tf
@@ -22,6 +22,17 @@ resource "aws_subnet" "prod_subnet_1" {
 }
 
 
+resource "aws_subnet" "prod_subnet_2" {
+  vpc_id            = data.aws_vpc.prod_vpc.id
+  availability_zone = data.aws_availability_zones.working.names[1]
+  cidr_block        = "10.0.1.0/28"
+  tags = {
+    Name    = "Subnet-1 in ${data.aws_availability_zones.working.names[1]}"
+    Account = "Subnet in account ${data.aws_caller_identity.current.account_id}"
+    Region  = data.aws_region.current.description
+  }
+
+}
 
 output "prod_vpc_id" {
   value = data.aws_vpc.prod_vpc.id
